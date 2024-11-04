b00e42bb01356eedb1f6e6ab7788baccb82e3800
ivedantsharma <vedantsharma102004@gmail.com>
2024-11-05 00:30:30 +0530

    added comment, tweet, like, playlist model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -172,7 +172,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: { refreshToken: undefined },
+      $unset: { refreshToken: 1 }, // this removes the field from document
     },
     {
       new: true,
